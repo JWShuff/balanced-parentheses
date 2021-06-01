e0b9d23dbc3b6e54e641a295648af8d3eb290791
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-01 09:55:53 -0400

    Unittests implemented, rework with Ankur during lecture

--- python/balance_parens_spec.py
+++ python/balance_parens_spec.py
@@ -1,9 +1,36 @@
 import unittest
 from balance_parens import balance_parens
 
-# Add more test cases!...
-print(balance_parens("abc(d)e(fgh))(i)j)k") == "abc(d)e(fgh)(i)jk")
-print(balance_parens("abc((d)e(fgh)(i)j(k") == "abc(d)e(fgh)(i)jk")
+class TestParens(unittest.TestCase):
 
-# Challenge: nested parentheses...
-print(balance_parens("abc(d)(ef(g(h))ij)k)lm()o)p") == "abc(d)(ef(g(h))ij)klm()op")
+    """
+    When you call Balanced Parens you get a string back
+    """
+    def test_returns_a_string(self):
+        self.assertTrue(type(balance_parens("test", output)) == list)
+
+    """
+    When you call balanced parens, you get correctly balanced strings
+    """
+    def test_function_output_accuracy(self):
+        self.assertTrue(balance_parens("abc(d)e(fgh))(i)j)k") == "abc(d)e(fgh)(i)jk")
+        self.assertTrue(balance_parens("abc((d)e(fgh)(i)j(k") == "abc(d)e(fgh)(i)jk")
+
+        self.assertTrue(balance_parens("()") == "()")
+        self.assertTrue(balance_parens("a(b)c)") == "a(b)c")
+        self.assertTrue(balance_parens("(a)(bdd)c)")  == "(a)(bdd)c")
+        self.assertTrue(balance_parens("a(b)(c)())")  == "a(b)(c)()")
+        self.assertTrue(balance_parens(")(")  == "")
+        self.assertTrue(balance_parens("(((((")  == "")
+        self.assertTrue(balance_parens(")(())(")  == "(())")
+        self.assertTrue(balance_parens("(()()(")  == "()()")
+        self.assertTrue(balance_parens(")())(()()(")  == "()()()")
+    
+    """
+    Challenge Question
+    """
+    def test_function_challenge(self):
+        self.assertTrue(balance_parens("abc(d)(ef(g(h))ij)k)lm()o)p") == "abc(d)(ef(g(h))ij)klm()op")
+
+if __name__ == '__main__':
+    unittest.main()
